d8b2d75d23650fb96e4d8f1b04cfb285602cdf89
Manoj Kumar S <manojkumars@sahajsoft.com>
2020-05-19 19:03:44 +0530

    Removes the unneccesary files from git

--- dist/routes/example.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const exampleRouter = express_1.Router();
-exampleRouter.get('/', (req, res) => {
-    res.send('Example Route Works!!!');
-});
-exports.default = exampleRouter;
-//# sourceMappingURL=example.js.map
