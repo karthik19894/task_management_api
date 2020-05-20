40817bcb534b31ff2b762b782172858d3a67baae
kesavanpalani <kesavanpalanipk@gmail.com>
2020-05-20 08:56:59 +0530

    Setup mongoose

---
+++ src/dtos/label.ts
@@ -0,0 +1,4 @@
+export class Label {
+    id: number;
+    name: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+
 dotenv.config();
-
-import express from 'express';
-import exampleRoute from "./routes/example";
-import mongoose from 'mongoose';
-
 const app = express();
-const port = (process.env.SERVER_PORT || 3001); // default port to listen
+const port = process.env.SERVER_PORT || 5000; // default port to listen
+const uri = process.env.ATLAS_URI;
 
-// const db = mongoose.connect(process.env.MONGO_URL);
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("mongo connection established successfully"))
+  .catch((err: any) => console.log(err));
 
-app.use("/example", exampleRoute);
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
-// start the Express server
 app.listen(port, () => {
-  // tslint:disable-next-line:no-console
   console.log(`hoooHooo!! server started at http://localhost:${port}`);
 });
