a5a2d2091c476b47bf1749e12b630456909d518c
Manoj Kumar S <kumarmanoj1158@gmail.com>
2020-05-30 21:40:31 +0530

    Sets user id and name in the cookie

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,13 +5,22 @@ import config from '../config'
 const authRouter = Router();
 
 authRouter.get('/token', async (req, res) => {
-    const result = await getToken(String(req.query.code))
-    res.status(201)
-        .cookie('auth_token', result, {
-            httpOnly: true,
-            domain: config.COOKIE_DOMAIN
-        })
-    res.send();
+    try {
+        const result = await getToken(String(req.query.code))
+        const { id_token, userId, name, exp } = result;
+        const cookieConfig = { domain: config.COOKIE_DOMAIN, expires: new Date(exp * 1000) }
+        res.status(201)
+            .cookie('auth_token', id_token, {
+                httpOnly: true,
+                ...cookieConfig
+
+            })
+            .cookie('user_id', userId, cookieConfig)
+            .cookie('user_name', name, cookieConfig)
+        res.send();
+    } catch (err) {
+        res.status(401).send("Login failed");
+    }
 });
 
 authRouter.get('/login', (req, res) => {
