9425fe84a57d2597d7169b185ae58ab5baba5301
Manoj Kumar S <kumarmanoj1158@gmail.com>
2020-05-31 00:11:24 +0530

    Adds with https

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ const dev = {
 }
 
 const prod = {
-    COOKIE_DOMAIN: 'eazytasks.herokuapp.com'
+    COOKIE_DOMAIN: 'https://eazytasks.herokuapp.com'
 }
 
 const config = process.env.NODE_ENV === 'production'
