9f12a6740ae9d7c9cb60301179ef25fecaae9a90
Manoj Kumar S <kumarmanoj1158@gmail.com>
2020-05-31 13:50:44 +0530

    Returns the token info in the token endpoint itself

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -17,7 +17,7 @@ authRouter.get('/token', async (req, res) => {
             })
             .cookie('user_id', userId, cookieConfig)
             .cookie('user_name', name, cookieConfig)
-            .send("Login succeeded")
+            .send({ idToken: id_token, userId, name })
     } catch (err) {
         res.status(401).send("Login failed");
     }
