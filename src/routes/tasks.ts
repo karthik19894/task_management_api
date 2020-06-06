2b6b192620e0f8437f00e0ea8296248f7e6f6b65
kesavanpalani <kesavanpalanipk@gmail.com>
2020-06-06 12:08:47 +0530

    Rename the authtoken

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,37 +1,37 @@
 import express = require("express");
 import { TasksService } from "../services/tasks-service";
-import {verifyToken} from "../services/googleOAuth2"
+import { verifyToken } from "../services/googleOAuth2"
 import { Task } from "../dtos/task";
 
 
 const tasksRouter = express.Router();
 
 tasksRouter.get("/", async (req, res) => {
-    try {
-      const category = req.body.category;
-      const authToken = req.cookies.auth_token; 
-      const userId = await verifyToken(authToken);
-      const tasks = await TasksService.findByUser(userId, category);
-      res.status(200).send(tasks);
-    } catch (err) {
-      console.log(err);  
-    }
-  });
+  try {
+    const category = req.body.category;
+    const authToken = req.cookies.authToken;
+    const userId = await verifyToken(authToken);
+    const tasks = await TasksService.findByUser(userId, category);
+    res.status(200).send(tasks);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
- tasksRouter.post("/", async (req, res) => {
-    try {
-        const task : Task = req.body;
-        const authToken = req.cookies.auth_token; 
-        const userId = await verifyToken(authToken);
-        task.userId = userId
-        const response = await TasksService.save(task);
-        res.status(200).send(response);
-      } catch (err) {
-        console.log(err);
-      }
- });
- 
- tasksRouter.delete("/:taskId", async (req, res) => {
+tasksRouter.post("/", async (req, res) => {
+  try {
+    const task: Task = req.body;
+    const authToken = req.cookies.authToken;
+    const userId = await verifyToken(authToken);
+    task.userId = userId
+    const response = await TasksService.save(task);
+    res.status(200).send(response);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+tasksRouter.delete("/:taskId", async (req, res) => {
   try {
     const taskId = req.params.taskId;
     const response = await TasksService.delete(taskId);
@@ -40,17 +40,17 @@ tasksRouter.get("/", async (req, res) => {
     console.log(err);
   }
 
- });
+});
 
- tasksRouter.put("/:taskId", async (req, res) => {
-   try {
-     const taskId = req.params.taskId;
-     const task : Task = req.body;
-     const response = await TasksService.update(taskId, task);
-     res.status(200).send(response);
-   } catch(err){
-     console.log(err);
-   }
- });
+tasksRouter.put("/:taskId", async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    const task: Task = req.body;
+    const response = await TasksService.update(taskId, task);
+    res.status(200).send(response);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
- export default tasksRouter;
+export default tasksRouter;
